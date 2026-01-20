e445ac6a06b68a6d043c2667b5863ec037e7d40b
Adityarai122 <dashingaditya4@gmail.com>
2026-01-20 19:49:28 +0530

    done

--- backend/src/config/env.config.js
+++ backend/src/config/env.config.js
@@ -6,7 +6,7 @@ const _env = {
     PORT: process.env.PORT || 8000,
     MONGODB_URI: process.env.MONGODB_URI,
     NODE_ENV: process.env.NODE_ENV,
-    CORS_ORIGIN: process.env.CORS_ORIGIN || 'http://localhost:5173'
+    CORS_ORIGIN: process.env.CORS_ORIGIN 
 };
 
 export const { PORT, MONGODB_URI, NODE_ENV, CORS_ORIGIN } = _env;
